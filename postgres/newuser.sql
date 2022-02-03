09e3bef830ae1e386b992644130154230240558b
Rusty Hermansen <dr.delight@gmail.com>
2022-02-02 23:10:35 -0700

    added sequence permissions

--- postgres/newuser.sql
+++ postgres/newuser.sql
@@ -1,4 +1,4 @@
 CREATE ROLE wg_admin LOGIN PASSWORD 'sleepyjackthefiredrill' NOINHERIT;
 GRANT USAGE ON SCHEMA wireguard TO wg_admin;
-GRANT CONNECT ON DATABASE postgres TO wg_admin;
+GRANT USAGE, SELECT ON SEQUENCE wireguard.client_client_id_seq TO wg_admin;
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA wireguard TO wg_admin;
