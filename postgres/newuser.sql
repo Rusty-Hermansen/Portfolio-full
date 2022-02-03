55f033caae83758687b7beb36f1b1870f4de6b2f
Rusty Hermansen <dr.delight@gmail.com>
2022-02-02 23:03:09 -0700

    fixed small error

--- postgres/newuser.sql
+++ postgres/newuser.sql
@@ -1,4 +1,4 @@
 CREATE ROLE wg_admin LOGIN PASSWORD 'sleepyjackthefiredrill' NOINHERIT;
 GRANT USAGE ON SCHEMA wireguard TO wg_admin;
-GRANT CONNECT ON DATABASE wireguard TO wg_admin;
+GRANT CONNECT ON DATABASE postgres TO wg_admin;
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA wireguard TO wg_admin;
