b7cfb81d172ed18703c8b4acf5bc0159a6a5d158
Rusty-Hermansen <highperformancemustangs@gmail.com>
2022-02-11 01:23:06 -0700

    st

--- React/src/components/SinglePost.js
+++ React/src/components/SinglePost.js
@@ -36,9 +36,9 @@ const SinglePost = () => {
             <section className="justify-center min-h-screen pt-12 lg:pt-36 px-8 bg-black bg-opacity-75">
                         <div className=" container text-white">
                             <div className="row">
-                            <h1 className="text-3xl text-white font-bold leading-none lg:leading-snug home-name">
-                   <span className="text-yellow-600">{dbPost?.title?? ""}</span>
-                </h1>
+                            
+                   <h2 className="text-yellow-600 text-3xl py-2 px-3 font-bold">{dbPost?.title?? ""}</h2>
+             
                                
                                 <div className="col-12 p-5 bg-black bg-opacity-50">
                                     {parse(dbPost?.post ?? "")}
