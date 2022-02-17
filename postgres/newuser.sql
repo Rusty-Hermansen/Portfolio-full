dabc5d605b884c20c0fc4ebcf4a1b95170e56b4b
Rusty-Hermansen <highperformancemustangs@gmail.com>
2022-02-17 00:48:35 -0700

    tables

--- postgres/newuser.sql
+++ postgres/newuser.sql
@@ -1,5 +1,5 @@
 CREATE ROLE wg_admin LOGIN PASSWORD 'sleepyjackthefiredrill' NOINHERIT;
 GRANT USAGE ON SCHEMA wireguard TO wg_admin;
-GRANT USAGE, SELECT ON SEQUENCE wireguard.client_client_id_seq TO wg_admin;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA wireguard TO wg_admin;
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA wireguard TO wg_admin;
 
