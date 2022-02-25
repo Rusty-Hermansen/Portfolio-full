b1f04df751736c9bd32acedb29d2cdd58d49f019
Rusty-Hermansen <highperformancemustangs@gmail.com>
2022-02-25 01:17:46 -0700

    username

--- React/src/components/Secure.js
+++ React/src/components/Secure.js
@@ -1,33 +1,40 @@
 import axios from 'axios'
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 const Secure = () => {
-const [userName, setUserName] = useState('')
+    const [userName, setUserName] = useState('')
 
 
-useEffect(() => {
-let history = useHistory();
- axios.get('/api/auth/secure', {withCredentials: true})
- .then(r => {
-     console.log(r)
-     if(r.response.status === 301){
-        history.push("/login")
-     }
-    
-     setUserName(r.data)})
- .catch(err => {
-     console.error(err)
-     if (err.response?.status===301){
-         history.push("/login")
-     }
- })
-}, [])
+    useEffect(() => {
+        let history = useHistory();
+        axios.get('/api/auth/secure', { withCredentials: true })
+            .then(r => {
+                console.log(r)
+                if (r.response.status === 301) {
+                    history.push("/login")
+                }
 
-    return(
-        <div>
+                setUserName(r.data)
+            })
+            .catch(err => {
+                console.error(err)
+                if (err.response?.status === 301) {
+                    history.push("/login")
+                }
+            })
+    }, [])
 
-        </div>
-    )
+    if (userName) {
+        return (<div>
+            <h1>Welcome, {userName}!</h1>
+        </div>)
+    }
+    else {
+        return (
+            <div>
+
+            </div>)
+    }
 }
 
 export default Secure;
