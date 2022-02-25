683e5d14e37447c084987096c2c7f54be3702647
Rusty-Hermansen <highperformancemustangs@gmail.com>
2022-02-25 00:22:30 -0700

    asdf

--- React/src/components/Secure.js
+++ React/src/components/Secure.js
@@ -5,7 +5,13 @@ const [userName, setUserName] = useState('')
 
 useEffect(() => {
  axios.get('/api/auth/secure', {withCredentials: true})
- .then(r => setUserName(r.data))
+ .then(r => {
+     console.log(r)
+     if(r.response.status === 301){
+        history.pushState("/login")
+     }
+    
+     setUserName(r.data)})
  .catch(err => {
      console.error(err)
      if (err.response?.status===301){
