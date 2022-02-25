dc9f87fc8ad91ad44bdace82c65c0851f0e3ea85
Rusty-Hermansen <highperformancemustangs@gmail.com>
2022-02-25 00:32:53 -0700

    history:

--- React/src/components/Secure.js
+++ React/src/components/Secure.js
@@ -1,21 +1,23 @@
 import axios from 'axios'
-import {useState, useEffect} from 'react'
+import {useState, useEffect} from 'react';
+import { useHistory } from 'react-router';
 const Secure = () => {
 const [userName, setUserName] = useState('')
+const history = useHistory();
 
 useEffect(() => {
  axios.get('/api/auth/secure', {withCredentials: true})
  .then(r => {
      console.log(r)
      if(r.response.status === 301){
-        history.pushState("/login")
+        history.push("/login")
      }
     
      setUserName(r.data)})
  .catch(err => {
      console.error(err)
      if (err.response?.status===301){
-         history.pushState("/login")
+         history.push("/login")
      }
  })
 }, [])
