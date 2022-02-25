1b52e6caa2304963b85fa957b62ecbeec3383328
Rusty-Hermansen <highperformancemustangs@gmail.com>
2022-02-24 20:37:42 -0700

    Going for the cookies again, issue with not adding time in db query

--- backend/App.js
+++ backend/App.js
@@ -44,7 +44,9 @@ app.post('/api/auth/login', async (req, res) => {
         }
         const session_id = uuid()
         console.log("session " + session_id)
-        await queries.storeSession(session_id, dbResult.user_id);
+        const time = date.now();
+        time.setHours( time.getHours() + 2 );
+        await queries.storeSession(session_id, dbResult.user_id, time);
         res.cookie("session_id", session_id, { sameSite: 'strict', httpOnly: true })
         res.send(200)
     }
