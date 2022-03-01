4f14f5aaf6c2643eb5ce94d80d634a1fd13141ab
Rusty-Hermansen <highperformancemustangs@gmail.com>
2022-02-28 20:44:51 -0700

    logging authservice

--- React/src/Services/authService.js
+++ React/src/Services/authService.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 const apiUrl = '/api/auth'
 
 const signIn = async(username, password) => {
+    console.log("hitting sign in: " + username + " passwrod: " + password)
     const res = await axios.post(apiUrl + '/login', {username, password})
 
 }
