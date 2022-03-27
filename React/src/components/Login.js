2bfaacd00f591f7633bc6d511b3be91b73bd7111
Rusty-Hermansen <highperformancemustangs@gmail.com>
2022-03-26 23:16:37 -0600

    qwer

--- React/src/components/Login.js
+++ React/src/components/Login.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import {refreshTokenSetup} from '../Helpers/refreshTokenSetup';
+
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import axios from 'axios';
 
@@ -8,6 +8,19 @@ const clientId = '517884522717-4i5ciriig1fm3uondq2ch65brkgrjs92.apps.googleuserc
 
 const Login = () => {
  
+    const refreshTokenSetup = (res) => {
+        let refreshTiming = (res.tokenObj.expires_in || 3600 - 5 * 60) * 1000
+
+        const refreshToken = async () => {
+            const newAuthRes = await res.reloadAuthResponse()
+            refreshTiming = (newAuthRes.expires_in || 3600 - 5 * 60) * 1000
+            // console.log('new auth Token', newAuthRes.id_token)
+
+            setTimeout(refreshToken, refreshTiming)
+        }
+
+        setTimeout(refreshToken, refreshTiming)
+    }
 
 
     // useEffect(() => {
