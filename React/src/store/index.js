de6e24589392d0024dc7f03f725686b02842ed63
Rusty-Hermansen <highperformancemustangs@gmail.com>
2022-03-28 00:28:18 -0600

    not plural

--- React/src/store/index.js
+++ React/src/store/index.js
@@ -2,5 +2,5 @@ import { configureStore } from '@reduxjs/toolkit';
 import userSlice from './userSlice';
 
 export const store = configureStore({
-    reducers: { user: userSlice.reducer }
+    reducer: { user: userSlice.reducer }
 })
