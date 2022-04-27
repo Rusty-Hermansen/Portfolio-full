63fd44b4377473fd2bef1f626c14449920b901da
Rusty-Hermansen <highperformancemustangs@gmail.com>
2022-04-26 21:43:44 -0600

    trying a post out

--- React/src/components/Login.js
+++ React/src/components/Login.js
@@ -80,12 +80,79 @@ const Login = () => {
         alert('You have logged out')
     }
 
-    const markdown = `## Title
-here is some content it looks good because it's content. I like it
-### Here is more markdown stuff
-lets see if it renders correctly
-### stuff
-more stuff`
+    const markdown = `Progressing toward earning my Bachelors degree and being immersed in the world of
+tech, I have learned and heard about all sorts of different technologies. One of those technologies that has
+caught my attention is kubernetes, or as it is commonly known, k8s. Working as a DevOps engineer intern for
+Pluralsight, I had an opportunity to learn about k8s and even use it to some degree. I wanted to learn more
+about it and here you can join my journey in learning kubernetes. 
+
+![4](https://github.com/Rusty-Hermansen/portfolio/blob/main/src/posts/Kubernetes/images/image4.png?raw=true)
+    
+To start our journey into kubernetes, we'll use an image that I previously
+pushed up to docker hub, my car-app image. 
+
+![2](https://github.com/Rusty-Hermansen/portfolio/blob/main/src/posts/Kubernetes/images/image2.png?raw=true)
+   
+This is the simple kubernetes manifest that I made. I indicated that this is a stable
+release of a pod, gave it a name and a label in the metadata portion, indicated what image I want to put in
+each pod and what the pod names should be. Line 13 indicates that I want to be able to access the pod on
+port 3000. 
+
+![12](https://github.com/Rusty-Hermansen/portfolio/blob/main/src/posts/Kubernetes/images/image12.png?raw=true)
+   
+It was then time to test my manifest, so I installed and started minikube and applied
+my manifest. I found that instead of just running kubectl get pods over and over again, if you run it with
+the --watch flag, it will show the pod status that updates periodically.
+
+![5](https://github.com/Rusty-Hermansen/portfolio/blob/main/src/posts/Kubernetes/images/image5.png?raw=true)
+    
+I also wanted to make some services, this NodePort service indicates a port that is
+open on every node in my kubernetes cluster. It also indicates the port the service listens on in the
+cluster, the port the app in the container is listening on (this needs to match the manifest), the actual
+indicated nodeport, and the label selector that must match the label in the manifest file.
+
+![9](https://github.com/Rusty-Hermansen/portfolio/blob/main/src/posts/Kubernetes/images/image9.png?raw=true)
+
+Here I applied my nodeport service and looked at the details of my service.
+
+![6](https://github.com/Rusty-Hermansen/portfolio/blob/main/src/posts/Kubernetes/images/image6.png?raw=true)
+
+I also wanted to be able to have multiple pods that a load balancer can direct
+traffic to, so I opted to make a simple load balancer. I just indicated all of the normal metadata, stated I
+want a load balancer, had it listen internally on port 80 and targetport that the service sends requests to
+that the pods listen on. 
+
+![3](https://github.com/Rusty-Hermansen/portfolio/blob/main/src/posts/Kubernetes/images/image3.png?raw=true)
+
+Here we can see the LoadBalancer up and running.
+
+![7](https://github.com/Rusty-Hermansen/portfolio/blob/main/src/posts/Kubernetes/images/image7.png?raw=true)
+
+
+Since I had all of the services that I wanted in place, it was time to exercise what
+kubernetes is capable of in my deployment manifest. 
+
+![10](https://github.com/Rusty-Hermansen/portfolio/blob/main/src/posts/Kubernetes/images/image10.png?raw=true)
+
+Here I&rsquo;m applying my deployment manifest, as you can see here, the three pods
+are created successfully. 
+
+![11](https://github.com/Rusty-Hermansen/portfolio/blob/main/src/posts/Kubernetes/images/image11.png?raw=true)
+
+
+Here I decided to test the self healing capabilities of kubernetes. In the context of
+servers being cattle not pets, I decided to kill one. Kubernetes automatically made a new pod to replace the
+old one.
+
+![1](https://github.com/Rusty-Hermansen/portfolio/blob/main/src/posts/Kubernetes/images/image1.png?raw=true)
+
+I also wanted to be able to access whatever lives in my pods, in my case, my simple
+react app that allows you to post a car that you want to sell. Since I used minikube as my kubernetes
+playground, I created a service to access the load balancer so I could hit the react app in my pods. </span>
+
+![8](https://github.com/Rusty-Hermansen/portfolio/blob/main/src/posts/Kubernetes/images/image8.png?raw=true)
+
+Here is the running app that lives in each pod of my cluster.`
      
     if (!isLoggedIn) {
         return (
